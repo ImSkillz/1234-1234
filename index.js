866b267f09cb11a3633e83bcb34708a3a4be7fa3
ImSkillz <42804458+ImSkillz@users.noreply.github.com>
2018-10-13 23:57:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+console.log("BOT ONLINE");
+
+
+client.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('سباام يولد يلعن اومم الفله')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+client.login("NDc0MzA1Njk4Nzg4ODAyNTcx.DqIqXw.16W5VhPmwVj3Hzru57qObreC_84");
